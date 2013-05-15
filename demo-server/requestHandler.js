631338c7658b25e9a07db0adb8b79c792c4abc02
Tyler Speakman <tyler.speakman@gmail.com>
2013-05-15 10:33:17 -0400

    Refactored modules (internal)

--- demo-server/requestHandler.js
+++ demo-server/requestHandler.js
@@ -1,165 +1,166 @@
-  var fs = require("fs"),
-      qs = require("querystring");
+var fs = require("fs"),
+    qs = require("querystring");
 
-  var isDebug = true;
+var isDebug = true;
 
-  var
-   readDir = function(response, request) {
-      var parameters = qs.parse(request.url.split("?")[1]);
-	  log("Request handler 'readDir' was called. "+"(" + parameters+")");	  
-	  
-	if(parameters.path.lastIndexOf("\\")!=parameters.path.length-1) {           response.writeHead(200, {
-              "Content-Type": "text/json"
-          }); response.end(JSON.stringify({error:"Invalid path. Must end with backslash"}))}
-	  
-      /*      var parent = {
-          path: "C:\\Users\\tyler.speakman\\Downloads\\",
-          name: "Jordan Peterson",
-          isDirectory: true,
-          isAccessible: true
-      };
-      */
-var path = parameters.path || "",name = parameters.name || "\\", depth=parameters.depth || 0;
-      var parent = {
-          path: path,
-          name:  name,
-          isDirectory: true,
-          isAccessible: true
-      };
-      readdirRecursive(parent, depth, function() {
-          log("Complete");
-		  response.statusCode = 200;
-		  response.setHeader("Content-Type", "text/json");
-          response.end(JSON.stringify(parent));
-      });
+var
+ readDir = function (response, request) {
+     var parameters = qs.parse(request.url.split("?")[1]);
+     log("Request handler 'readDir' was called. " + "(" + parameters + ")");
 
-      function readdirRecursive(parent, depth, callback) {
-          //if (depth == 0) {
-          log(logParent);
-          //}
-          if (depth-- >= 0 && parent.isDirectory && parent.isAccessible) {
-              var fullParentPath = parent.path + parent.name + "\\"
-              fs.readdir(fullParentPath, function(err, files) {
-                  if (err) {
-                      parent.isAccessible = false;
-                      callback();
-                  }
-                  else {
-                      parent.children = parent.children || [];
-                      var numberOfRequests = files.length;
-                      log(logQueuedChildren);
-                      if (numberOfRequests === 0) {
-                          // If there are no children, then exit.
-                          callback();
-                      }
-                      else {
-                          // If there are children, then process them
-                          var index = files.length;
-                          while (index--) {
-                              var fileName = files[index];
-                              var child = {
-                                  path: fullParentPath,
-                                  name: fileName
-                              };
-                              parent.children.push(child);
-                              processChild(child)
+     if (parameters.path.lastIndexOf("\\") != parameters.path.length - 1) {
+         response.writeHead(200, { "Content-Type": "text/json" });
+         response.end(JSON.stringify({ error: "Invalid path. Must end with backslash" }));
+     }
 
-                              function processChild(child) {
-								child.isUpDir= false;
-                                  fs.stat(child.path + child.name, function(err, stats) {
-                                      if (err) {
-                                          child.isAccessible = false;
-                                          child.isDirectory = true;
-                                          // Not accessible -- don't go deeper
-                                          //trackRequests();
-                                      }
-                                      else {
-                                          child.isAccessible = true;
-                                              child.isDirectory = false;
-											  child.isFile= false; 										  
-                                          //log(JSON.stringify(child) +", "+stats.isDirectory());
-                                          if (stats.isFile()) {
-											  var unitOfSize = "B"
-											  var size = stats.size | 0;											  
-											  while((size/1024 | 0)>0){
-												
-												if(unitOfSize ==="B"){unitOfSize ="kB";}
-												else if(unitOfSize ==="kB"){unitOfSize ="MB";}
-												else if(unitOfSize ==="MB"){unitOfSize ="GB";}
-												else{break;}
-												size=size/1024 | 0;
-												console.log(size)
-											  }
-											  child.size = size +" "+unitOfSize;
-											  child.isFile= true; 
-											  child.lastModifiedDate = stats.mtime;											  
-                                          }
-										  else if (stats.isDirectory()) {
-												child.isDirectory= true; 
-										  }
-                                          else {
-                                          }
-                                      }
+     var path = parameters.path || "", name = parameters.name || "\\", depth = parameters.depth || 0;
+     var parent = { path: path, name: name, isDirectory: true, isAccessible: true };
+     readdirRecursive(parent, depth, function () {
+         log("Complete");
+         response.statusCode = 200;
+         response.setHeader("Content-Type", "text/json");
+         response.end(JSON.stringify(parent));
+     });
 
-                                      readdirRecursive(child, depth, trackRequests);
-                                  });
-                              }
-                          }
-                      }
+     //#region Internal Methods
 
-                      function trackRequests() {
-                          numberOfRequests--;
-                          log(logProcessedChildren);
-                          if (numberOfRequests === 0) {
-                              callback();
-                          }
-                      }
+     function readdirRecursive(parent, depth, callback) {
+         log(logParent);
+         if (depth-- >= 0 && parent.isDirectory && parent.isAccessible) {
+             var fullParentPath = parent.path + parent.name + "\\";
+             fs.readdir(fullParentPath, function (err, files) {
+                 if (err) {
+                     parent.isAccessible = false;
+                     callback();
+                 }
+                 else {
+                     parent.children = parent.children || [];
+                     var numberOfRequests = files.length;
+                     log(logQueuedChildren);
+                     if (numberOfRequests === 0) {
+                         // If there are no children, then exit.
+                         callback();
+                     }
+                     else {
+                         // If there are children, then process them
+                         var index = files.length;
+                         while (index--) {
+                             var fileName = files[index];
+                             var child = { path: fullParentPath, name: fileName };
+                             parent.children.push(child);
+                             processChild(child);
+                         }
 
-                      function logQueuedChildren() {
-                          return pad("    ", depth) + " " + "Queued children (" + numberOfRequests + ") (" + fullParentPath + ")";
-                      }
+                         //#region Internal Methods
 
-                      function logProcessedChildren() {
-                          return pad("    ", depth) + " " + "Processed children (" + numberOfRequests + ") (" + fullParentPath + ")";
-                      }
-                  }
-              });
-          }
-          else {
-              callback();
-          }
+                         function processChild(child) {
+                             child.isUpDir = false;
+                             fs.stat(child.path + child.name, function (err, stats) {
+                                 if (err) {
+                                     child.isAccessible = false;
+                                     child.isDirectory = true;
+                                 } else {
+                                     child.isAccessible = true;
+                                     child.isDirectory = false;
+                                     child.isFile = false;
+                                     if (stats.isFile()) {
+                                         child.size = formatFileSize(stats.size);
+                                         child.isFile = true;
+                                         child.lastModifiedDate = stats.mtime;
+                                     } else if (stats.isDirectory()) {
+                                         child.isDirectory = true;
+                                     } else {
+                                     }
+                                 }
 
-          function logParent() {
-              return pad("    ", depth) + " " + JSON.stringify(parent);
-          }
-      }
-  }, start = function(response) {
-      var body;
-      log("Request handler 'start' was called");
-      body = "<!doctype html>\n<html>\n<head>\n<meta http-equiv=\"Content-Type\" content=\"text/html\" charset=UTF-8 />\n</head>\n<body></body>\n</html>";
-      response.writeHead(200, {
-          "Content-Type": "text/html"
-      });
-      response.write(body);
-      return response.end();
-  };
+                                 readdirRecursive(child, depth, trackRequests);
+                             });
 
-  function pad(string, count) {
-      return (new Array(count + 2)).join(string)
-  }
+                             function formatFileSize(size) {
+                                 var unitOfSize = "B";
+                                 while ((size / 1024 | 0) > 0) {
+                                     if (unitOfSize === "B") {
+                                         unitOfSize = "kB";
+                                     } else if (unitOfSize === "kB") {
+                                         unitOfSize = "MB";
+                                     } else if (unitOfSize === "MB") {
+                                         unitOfSize = "GB";
+                                     } else {
+                                         break;
+                                     }
+                                     size = size / 1024 | 0;
+                                 }
 
-  function log(message, isDebug) {
-      if (isDebug) {
-          if ((typeof message == "function")) {
-              // Allow the "message" to be a function. Ideally this avoids unneccessary execution -- does it??
-              console.log(message());
-          }
-          else {
-              console.log(message);
-          }
-      }
-  }
+                                 return size + " " + unitOfSize;
+                             }
 
-  exports.start = start;
+                             //#endregion
+                         }
+                     }
 
-  exports.readDir = readDir;
+                     //#region Internal Methods
+
+                     function trackRequests() {
+                         numberOfRequests--;
+                         if (numberOfRequests === 0) {
+                             callback();
+                         }
+                         log(logProcessedChildren);
+                     }
+
+                     function logQueuedChildren() {
+                         return pad("    ", depth) + " " + "Queued children (" + numberOfRequests + ") (" + fullParentPath + ")";
+                     }
+
+                     function logProcessedChildren() {
+                         return pad("    ", depth) + " " + "Processed children (" + numberOfRequests + ") (" + fullParentPath + ")";
+                     }
+
+                     //#endregion
+                 }
+             });
+         }
+         else {
+             callback();
+         }
+
+         //#region Internal Methods
+
+         function logParent() {
+             return pad("    ", depth) + " " + JSON.stringify(parent);
+         }
+
+         //#endregion
+     }
+
+     //#endregion
+ }, start = function (response) {
+     var body;
+     log("Request handler 'start' was called");
+     body = "<!doctype html>\n<html>\n<head>\n<meta http-equiv=\"Content-Type\" content=\"text/html\" charset=UTF-8 />\n</head>\n<body></body>\n</html>";
+     response.writeHead(200, {
+         "Content-Type": "text/html"
+     });
+     response.write(body);
+     return response.end();
+ };
+
+function pad(string, count) {
+    return (new Array(count + 2)).join(string)
+}
+
+function log(message, isDebug) {
+    if (isDebug) {
+        if ((typeof message == "function")) {
+            // Allow the "message" to be a function. Ideally this avoids unneccessary execution -- does it??
+            console.log(message());
+        }
+        else {
+            console.log(message);
+        }
+    }
+}
+
+exports.start = start;
+
+exports.readDir = readDir;
--- demo-server/router.js
+++ demo-server/router.js
@@ -1,11 +1,10 @@
-var route = function(handle, pathname, response, request) {
+var route = function (handle, pathname, response, request) {
     console.log("About to route a request for " + pathname);
     if (typeof handle[pathname] === 'function') {
         return handle[pathname](response, request);
-    }
-    else {
+    } else {
         console.log("No request handler found for " + pathname);
-        response.writeHead(404, {"Content-Type": "text/plain"});
+        response.writeHead(404, { "Content-Type": "text/plain" });
         response.write("404 not found");
         return response.end();
     }
